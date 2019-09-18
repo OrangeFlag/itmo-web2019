f8e6a798e4f2f29fcd18361c0ab1387b1db066e5
IMay <imay@localhost.localdomain>
2019-09-18 11:20:10 +0300

    add less preprocessor

--- js/global.js
+++ js/global.js
@@ -1,31 +1,59 @@
 const unsecureApiKey = "1c14e84db0be925cc69df123e5989ecc";
-const weatherAPI = `https://samples.openweathermap.org/data/2.5/weather?appid=${unsecureApiKey}`;
+const weatherAPI = `https://api.openweathermap.org/data/2.5/weather?APPID=${unsecureApiKey}`;
 
-function getWeather(city, country) {
-    return $.getJSON(weatherAPI + `&q=${city},${country}`, function (weather) {
-        const city_country = doT.template("City: {{=it.name}} <br> Country: {{=it.sys.country}}");
-        document.getElementById("city_country").innerHTML = city_country(weather);
+function drawWeather(weather) {
 
-        const time = doT.template("Time: {{=it.dt}}");
-        document.getElementById("time").innerHTML = time(weather);
+    const city_country = doT.template(`
+        {{? it == null}}
+        City not found
+        {{?? it.name }}
+        City: {{=it.name}}, {{=it.sys.country}}
+        {{??}}
+        {{?}}
+        `);
+    document.getElementById("city_country").innerHTML = city_country(weather);
 
-        const temp = doT.template("Temperature: {{=it.main.temp}}");
-        document.getElementById("temp").innerHTML = temp(weather);
+    // const time = doT.template("Time: {{=new Date(it.dt*1000).toISOString().slice(-13, -5)}}");
+    // document.getElementById("time").innerHTML = time(weather);
 
-        const clouds_percent = doT.template("Clouds: {{=it.clouds.all}} %");
-        document.getElementById("clouds_percent").innerHTML = clouds_percent(weather);
+    const temp = doT.template(`
+        {{? it && it.main && it.main.temp }}
+        Temperature: {{=it.main.temp}}°
+        {{?}}
+        `);
+    document.getElementById("temp").innerHTML = temp(weather);
 
-        const wind_speed = doT.template("Wind speed: {{=it.wind.speed}} m/s");
-        document.getElementById("wind_speed").innerHTML = wind_speed(weather);
+    const clouds_percent = doT.template(`
+        {{? it && it.clouds && it.clouds.all }}
+        Clouds: {{=it.clouds.all}}%
+        {{?}}
+        `);
+    document.getElementById("clouds_percent").innerHTML = clouds_percent(weather);
 
-        const visibility = doT.template("Visibility: {{=it.visibility}}");
-        document.getElementById("visibility").innerHTML = visibility(weather);
-    })
+    const wind_speed = doT.template(`
+        {{? it && it.wind && it.wind.speed }}
+        Wind speed: {{=it.wind.speed}}m/s
+        {{?}}
+        `);
+    document.getElementById("wind_speed").innerHTML = wind_speed(weather);
+
+    const visibility = doT.template(`
+        {{? it && it.visibility }}
+        Visibility: 
+        {{=it.visibility}}m
+        {{?}}
+        `);
+    document.getElementById("visibility").innerHTML = visibility(weather);
+
 }
 
-function drawWeather() {
-    let weather = getWeather(
-        document.getElementById("city").value,
-        document.getElementById("country").value
-    );
+function getWeather() {
+    const city = document.getElementById("city").value;
+
+    $.getJSON(weatherAPI + `&q=${city}&units=metric`, drawWeather)
+        .fail(function (jqxhr) {
+            if (jqxhr.status === 404) {
+                drawWeather(null)
+            }
+        })
 }
